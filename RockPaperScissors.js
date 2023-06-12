941bd3b593972166f4c3f606a648da7f648eb474
neessam moustafa <122574146+neesssam@users.noreply.github.com>
2023-06-12 16:50:19 -0400

    Add files via upload

---
+++ RockPaperScissors.js
@@ -0,0 +1,66 @@
+const getUserChoice = userInput => {
+  userInput = userInput.toLowerCase();
+  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+    return userInput;
+  } else if (userInput === 'bomb') {
+    return 'You Automatically Win!';
+  } else {
+    console.log('Error!');
+  }
+};
+
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return 'rock';
+      break;
+    case 1:
+      return 'paper';
+      break;
+    case 2:
+      return 'scissors';
+      break;
+  }
+}
+
+function determineWinner(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    return 'The game is a tie!';
+  }
+
+  if (userChoice === 'rock') {
+    if (computerChoice === 'paper') {
+      return 'The computer won!';
+    } else {
+      return 'You won!';
+    }
+  }
+
+  if (userChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+      return 'The computer won!';
+    } else {
+      return 'You won!';
+    }
+  }
+
+  if (userChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+      return 'The computer won!';
+    } else {
+      return 'You won!';
+    }
+  }
+}
+
+function playGame() {
+  let userChoice = getUserChoice('rock');
+  let computerChoice = getComputerChoice();
+  console.log(userChoice);
+  console.log(computerChoice);
+
+  console.log(determineWinner(userChoice, computerChoice));
+}
+
+playGame();
